2cecdce592a02307c7e62c066bdbbeca382ca6f8
Houston <houstoncallaway@gmail.com>
2022-12-09 11:18:31 -0600

    craiyon works!

--- craiyon.js
+++ craiyon.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 // set up discord client and required gatewaybits
-const { Client, GatewayIntentBits, messageLink, DiscordAPIError } = require("discord.js");
+const { Client, GatewayIntentBits, messageLink, DiscordAPIError, AttachmentBuilder } = require("discord.js");
 const discordClient = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -22,43 +22,32 @@ discordClient.on("ready", () => {
 
 // function for submitting image request and getting response
 const submitImage = async (prompt, user) => {
-    console.log(`${user} submitted image prompt: '${prompt}', waiting for response...`)
-    let result = await craiyon.generate({
+    console.log(`${user} submitted craiyon image prompt: '${prompt}', waiting for response...`)
+    const result = await craiyon.generate({
       prompt,
-    })
-    // TODO: response is base64, need to figure out how to get from base64 arrays to uploads to discord
-    const bufferImages = [];
-    result.images.forEach(image => {
-        bufferImages.push(image);
     });
+    console.log("Result received. Processing now.");
+    let bufferImages = [];
+    result.images.forEach(image => {
+        bufferImages.push(image.base64);
+    });
+    console.log('Added images to buffer')
     return bufferImages;
 }
 
 // check messages and trigger
 discordClient.on('messageCreate', async msg => {
-// const openai = new OpenAIApi(configuration);
     if (msg.content.startsWith('/craiyon ')) {
         // get the text
         const prompt = msg.content.slice(9);
-        const user = msg.author.username
-        console.log(`${user} submitted image prompt: '${prompt}', waiting for response...`)
-        let result = await craiyon.generate({
-          prompt,
-        })
-        // TODO: response is base64, need to figure out how to get from base64 arrays to uploads to discord
-        const bufferImages = [];
-        result.images.forEach(image => {
-            bufferImages.push(image);
-        });
+        const user = msg.author.username;
         // display response
-        submitImage(prompt, user);
-        let imageCounter = 0
-        bufferImages.forEach(image => {
-            const file = new discordClient.Attachment(Buffer.from(base64Data, "base64"), `${imageCounter}`);
-            msg.reply(file)
-            imageCounter++
-        })
-        .then((bufferImages) => {
+        submitImage(prompt, user).then((bufferImages) => {
+            console.log('Uploading images as files')
+            bufferImages.forEach(i => {
+                const file = new AttachmentBuilder(Buffer.from(i, "base64"), `${i.slice(0,12)}`);
+                msg.reply({files: [file]})
+            });
         console.log('Responded!')
         }).catch((error) => {
         console.log(error)
